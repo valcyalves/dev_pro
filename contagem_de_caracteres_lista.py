a2c9e7198346d31e2840b17643ddee2b54e59961
python_project <valcypapaiarthur@hotmail.com>
2023-01-22 15:13:12 +0000

    contagem_completa

---
+++ contagem_de_caracteres_lista.py
@@ -0,0 +1,39 @@
+def contar_caracteres(s):
+    """Funão que conta os caracteres de uma string
+
+    Ex:
+
+    >>> contar_caracteres('renzo')
+    e: 1
+    n: 1
+    o: 1
+    r: 1
+    z: 1
+    >>> contar_caracteres('banana')
+    a: 3
+    b: 1
+    n: 2
+
+    :param s: string a ser contada
+    :return:
+    """
+    caracteres_ordenados = sorted(s)
+
+    caracter_anterior = caracteres_ordenados[0]
+    contagem = 1
+
+    for caracter in caracteres_ordenados[1:]:
+        if caracter == caracter_anterior:
+            contagem += 1
+        else:
+            print(f'{caracter_anterior}: {contagem}')
+            caracter_anterior = caracter
+            contagem = 1
+
+    print(f'{caracter_anterior}: {contagem}')
+
+
+if __name__ == '__main__':
+    contar_caracteres('renzo')
+    print()
+    contar_caracteres('banana')
